8843fc4decf0a6958acb6a76d9301d29192cbb5f
Kaklamanis <victor.kaklamanis@accenture.com>
2024-11-07 12:49:34 +0200

    Fixed a few incorrect uses of pytest.mark.parametrize. Fixed issues of indentation in the main code. Issues with multiple slot meetings conflicts.

--- classes/ClDay.py
+++ classes/ClDay.py
@@ -6,7 +6,7 @@ class ClDay:
 
     def __init__(self, name="") -> None:
         self.name = name
-        self.timeline = {float(x): list() for x in np.arange(9.0, 18.5, 0.5)}
+        self.timeline = {float(x): list() for x in np.arange(9.0, 19.0, 0.5)}
 
     def import_meeting(self, name, meet_time, duration, owner) -> None:
 
@@ -31,50 +31,50 @@ class ClDay:
     def find_conflicts(self) -> dict:
 
         result = dict()
-
-        # result = { time_slot: { "PM": "pm_name" }, { "Meetings": [ meeting names, .. ] }, ... }
+        # result: { }
 
         concurrent_meetings = list()
         overbooked_pms = list()
 
         for time_slot, meeting_list in self.timeline.items():
+            """
+            timeslot: float
+            meeting_list: [ tuple('meet_name', meet_owner'), ... ]
+            """
 
             owners_in_current_timeslot = list()  # gets reset on every time slot iteration
             concurrent_meetings_in_current_timeslot = list()  # gets reset on every time slot iteration
 
-            # these are created here iteratively, as we cycle through all the indeces of the "timeline" dictionary of
-            # the current day
+            # the 3 variables below created here iteratively, as we cycle through all the indeces of the "timeline"
+            # dictionary of the current day
             result[time_slot] = dict()
             result[time_slot]["Meetings"] = list()
             result[time_slot]["PMs"] = list()
+            # result: { time_slot: { "PMs": [], "Meetings": [] }, }
 
             if len(meeting_list) > 1:
 
                 for meet_tuple in meeting_list:
-                    # [ (name, owner), (name2, owner2), ... ]
+                    """
+                    meet_tuple: (name, owner)
+                    """
+
                     meeting_name = meet_tuple[0]
                     meeting_owner = meet_tuple[1]
 
                     concurrent_meetings_in_current_timeslot.append(meeting_name)
                     owners_in_current_timeslot.append(meeting_owner)
 
-                    if len(concurrent_meetings_in_current_timeslot) > 1:  # no reason to run iteration if list is empty
+                if len(concurrent_meetings_in_current_timeslot) > 1:  # no reason to run iteration if list is empty
+                    for each_meeting in concurrent_meetings_in_current_timeslot:
+                        result[time_slot]["Meetings"].append(each_meeting)
 
-                        debug = 1
-                        for each_meeting in concurrent_meetings_in_current_timeslot:
+                overbooked_pms_in_this_timeslot = list(set([owner for owner in owners_in_current_timeslot if owners_in_current_timeslot.count(owner) > 1]))
 
-                            result[time_slot]["Meetings"].append(each_meeting)
-
-                            print(result[time_slot]["Meetings"])
-                            print(debug)
-                            debug += 1
-
-                    overbooked_pms_in_this_timeslot = list(set([owner for owner in owners_in_current_timeslot if owners_in_current_timeslot.count(owner) > 1]))
-
-                    if len(overbooked_pms_in_this_timeslot) > 0:  # no reason to run iteration if list is empty
-                        for pm in overbooked_pms_in_this_timeslot:
-                                if result.get(time_slot) is not None:
-                                    result[time_slot]["PMs"].append(pm)
+                if len(overbooked_pms_in_this_timeslot) > 0:  # no reason to run iteration if list is empty
+                    for pm in overbooked_pms_in_this_timeslot:
+                            if result.get(time_slot) is not None:
+                                result[time_slot]["PMs"].append(pm)
 
             else:
                 # No concurrent meetings
